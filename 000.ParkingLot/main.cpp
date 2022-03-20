8a7fffdeea0bb99a273d1274b84d963b95ced3fc
saugata-git <31741625+saugata-git@users.noreply.github.com>
2022-03-21 00:22:51 +0530

    Update main.cpp

--- 000.ParkingLot/main.cpp
+++ 000.ParkingLot/main.cpp
@@ -1,28 +1,25 @@
 #include"parkinglot.h"
 
-
 int main(){
-
-   ParkingLot parkingLot;
-   parkingLot.displayFreeSlots();
+        ParkingLot parkingLot;
+        parkingLot.displayFreeSlots();
    
 	Vehicle bike("pulser", small);
 	parkingLot.ParkVehicle(&bike);
 
 	parkingLot.displayFreeSlots();
 
-	Vehicle car("oddi", medium);
+	Vehicle car("nano", medium);
 	parkingLot.ParkVehicle(&car);
 	
 	bike.removeVehicle();
 
 	parkingLot.displayFreeSlots();
 
-	Vehicle bus("Mahindra", large);
+	Vehicle bus("mahindra", large);
 	parkingLot.ParkVehicle(&bus);
 
 	parkingLot.displayFreeSlots();
 
-   return 0;
-
+        return 0;
 }
